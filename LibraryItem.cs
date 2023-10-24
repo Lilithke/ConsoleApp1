f1fb3512cad048b2d1f8956264b3af9dcee783b9
Lilithke <bocsi.vi13@gmail.com>
2023-10-24 17:37:01 +0200

    Add project files.

---
+++ LibraryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class LibraryItem
+    {
+        string title;
+        string author;
+        bool isAvailable;
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public bool IsAvailable { get => isAvailable; set => isAvailable = value; }
+
+        public LibraryItem(string title, string author, bool isAvailable)
+        {
+            Title = title;
+            Author = author;
+            IsAvailable = isAvailable;
+        }
+        public string GetItemType() {  return title; }
+    }
+}
